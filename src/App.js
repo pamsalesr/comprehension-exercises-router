8875e6b14c36fcb181e2c86266b17583505dd98f
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-03-02 18:47:47 -0300

    Finish fixation exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/strict-access">StrictAcess</Link></li>
+          </ul> 
+        </nav>
+        <Switch>
+          {/* <Route exact path="/users" component={Users}/> */}
+          <Route 
+            exact
+            path="/users/:id"
+            render={(props) => <Users {...props} greetingMessage="Good Morning!" /> }
+          />
+          <Route exact path="/about" component={About}/>
+          <Route
+            exact
+            path="/strict-access" 
+            render={() => (
+              <StrictAccess user= { {username: 'joao', password: '1234'} } />
+            )}
+          />
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
